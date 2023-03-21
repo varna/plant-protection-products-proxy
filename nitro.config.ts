808970826935e819949d2af7a9b77dff9b1c6689
varna <paul.varna@gmail.com>
2023-03-21 17:51:33 +0200

    1 file modified

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,9 @@ import { defineNitroConfig } from 'nitropack';
 
 export default defineNitroConfig({
   routeRules: {
-    '/**': { proxy: 'https://agrobase.app/lithuania/**' },
+    // '/**': { proxy: 'https://agrobase.app/lithuania/**' },
+    '/**': {
+      redirect: { to: 'https://agrobase.app/lithuania/**', statusCode: 200 },
+    },
   },
 });
